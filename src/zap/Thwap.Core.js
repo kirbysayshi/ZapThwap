d68eda63bd4ee079b66fd005be157821d0bd693a
KirbySaysHi <kirbysayshi@gmail.com>
2010-12-06 01:16:26 -0500

    added instrumentation back into Thwap, for now

--- src/zap/Thwap.Core.js
+++ src/zap/Thwap.Core.js
@@ -24,14 +24,14 @@ function World(){
 World.prototype.addVertex = function(v){
 	if(this.vlist.indexOf(v) === -1){
 		this.vlist.push(v);
-		//THWAP.Instrumentation.library.maxVertices++;
+		THWAP.Instrumentation.library.maxVertices++;
 	}
 	return this;
 }
 World.prototype.addConstraint = function(c){
 	if(this.clist.indexOf(c) === -1){
 		this.clist.push(c);
-		//THWAP.Instrumentation.library.maxConstraints++;
+		THWAP.Instrumentation.library.maxConstraints++;
 	}
 	
 	this.addVertex(c.v1);
@@ -41,7 +41,7 @@ World.prototype.addConstraint = function(c){
 World.prototype.addBody = function(b){
 	if(this.blist.indexOf(b) === -1){
 		this.blist.push(b);
-		//THWAP.Instrumentation.library.maxBodies++;
+		THWAP.Instrumentation.library.maxBodies++;
 	}
 	
 	for(var i = 0; i < b.clist.length; i++){
@@ -61,7 +61,7 @@ World.prototype.step = function(dt){
 		,o = this.blist.length;
 	
 	// archive previous instrumentation data
-	//THWAP.Instrumentation.archiveThisStep();
+	THWAP.Instrumentation.archiveThisStep();
 	
 	// update verticies
 	while(v >= 0){
@@ -125,7 +125,7 @@ Vertex.prototype.collideConstraint = function(c){
 		) return this;
 	
 	// INSTRUMENTATION
-	//THWAP.Instrumentation.log('VCCollisionTests', [this, c]);
+	THWAP.Instrumentation.log('VCCollisionTests', [this, c]);
 	
 	//---------------------------------------------------------------------
 	// Attempt to discard quickly
@@ -210,7 +210,7 @@ Vertex.prototype.collideConstraint = function(c){
 	c.eventCallbacks.onVertexCollisionResponse.call(c, this, velocity, collisionDepth);
 	
 	// INSTRUMENTATION
-	//THWAP.Instrumentation.log('VCCollisionResponses', [this, c]);
+	THWAP.Instrumentation.log('VCCollisionResponses', [this, c]);
 	
 	return this;
 }
@@ -221,7 +221,7 @@ Vertex.prototype.collideVertex = function(vert){
 	// TODO: swap out with ov3 for just this method
 	
 	// INSTRUMENTATION
-	//THWAP.Instrumentation.log('VVCollisionTests', [this, vert]);
+	THWAP.Instrumentation.log('VVCollisionTests', [this, vert]);
 	
 	var  tcpos = this.cpos, vcpos = vert.cpos
 		,tppos = this.ppos, vppos = vert.ppos
@@ -277,7 +277,7 @@ Vertex.prototype.collideVertex = function(vert){
 	vert.eventCallbacks.onVertexCollisionResponse.call(vert, this, velColl);
 	
 	// INSTRUMENTATION
-	//THWAP.Instrumentation.log('VVCollisionResponses', [this, vert]);
+	THWAP.Instrumentation.log('VVCollisionResponses', [this, vert]);
 	
 	return this;
 }
@@ -311,7 +311,7 @@ Vertex.prototype.update = function(dt, ldt){
 	s.eventCallbacks.onPostUpdate.call(this);
 	
 	// INSTRUMENTATION
-	//THWAP.Instrumentation.log('VUpdates', this);
+	THWAP.Instrumentation.log('VUpdates', this);
 	
 	return this;
 }
@@ -429,7 +429,7 @@ LinearConstraint.prototype.update = function(dt){
 		.computeNormal();
 	this.eventCallbacks.onPostUpdate.call(this);
 	// INSTRUMENTATION
-	//THWAP.Instrumentation.log('CUpdates', this);
+	THWAP.Instrumentation.log('CUpdates', this);
 	return this;
 }
 LinearConstraint.prototype.debugDrawNormal = function(ctx, offset, scale){
@@ -723,7 +723,7 @@ Body.prototype.debugDraw = function(ctx, offset, scale){
 }
 
 var Instrumentation = {
-	sample: false
+	sample: true
 	,thisStep: {
 		 VVCollisionTests: []
 		,VCCollisionTests: []
